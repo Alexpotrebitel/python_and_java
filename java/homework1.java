de83711a54938d3e5ca7b513bb134ba3ec8913a3
Alexpotrebitel <118938387+Alexpotrebitel@users.noreply.github.com>
2023-05-04 21:50:56 +0500

    f

---
+++ java/homework1.java
@@ -0,0 +1,12 @@
+#Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+
+#Вывести все простые числа от 1 до 1000
+java
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Введите число n: ");
+int n = scanner.nextInt()
