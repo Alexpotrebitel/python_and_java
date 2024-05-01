626142e299d58536f9ae104036f6087a81ac60f6
Alexpotrebitel <118938387+Alexpotrebitel@users.noreply.github.com>
2024-05-01 15:02:46 +0500

    ff

--- java/семинары/seminar 4/Main2.java
+++ java/семинары/seminar 4/Main2.java
@@ -1,5 +1,4 @@
 
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,7 +11,7 @@ public class Main2 {
     нужно расплитить строку по~, сохранить text в связный список на позицию num
     Если введено print~num, выводит строку из позиции num и удаляет ее из списка
      */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         List<String> list = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
         boolean work = true;
@@ -34,24 +33,20 @@ public class Main2 {
                         list.add(num, text);
                 }
             } else {
-                System.out.println("РќРµРґРѕРїСѓСЃС‚РёРјС‹Р№ РёРЅРґРµРєСЃ. РЈРєР°Р¶РёС‚Рµ Р·РЅР°С‡РµРЅРёРµ РІ РґРёР°РїР°Р·РѕРЅРµ РѕС‚ 0 РґРѕ " + list.size());
+                System.out.println("Недопустимый индекс. Укажите значения от 0 до" + list.size());
             }
         }
     }
 
     private static void print(int num, List<String> list) {
         if (num == list.size()){
-            System.out.println("РќРµРґРѕРїСѓСЃС‚РёРјС‹Р№ РёРЅРґРµРєСЃ. РЈРєР°Р¶РёС‚Рµ Р·РЅР°С‡РµРЅРёРµ РІ РґРёР°РїР°Р·РѕРЅРµ РѕС‚ 0 РґРѕ "
+            System.out.println("Недопустимый индекс. Укажите значения от 0 до "
                     + (list.size() - 1));
         } else {
             System.out.println(list.remove(num));
         }
     }
 }
-
-
-
-
 
 
 
